1f85c2bad55bcc7108c2ebc3f000de98e3312c45
Phil <philip.schaefer0@gmail.com>
2019-12-26 21:15:00 +0100

    removed old functioncall

--- functions/EVENT_message.js
+++ functions/EVENT_message.js
@@ -18,7 +18,6 @@ module.exports.run = async (client, message, config) => {
   // check if server is on ParticipatingServers Table
   let serverID = null;
   if (message.channel.guild) serverID = message.channel.guild.id;
-  if (!await client.functions.get('FUNC_checkServer').run(serverID)) return;
 
   // remove prefix and lowercase
   let cmd = client.commands.get(command.slice(config.prefix.length).toLowerCase());
