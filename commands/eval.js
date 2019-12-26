978f8be9defb514cd33792d379a7d31f89f45a8a
Phil <philip.schaefer0@gmail.com>
2019-12-26 21:11:00 +0100

    renamed failsave

--- commands/eval.js
+++ commands/eval.js
@@ -6,7 +6,7 @@ const clean = (text) => {
 module.exports.run = async (client, message, args, config) => {
   const args_eval = message.content.split(' ').slice(1);
   if (message.author.id !== '172031697355800577') return message.react('❌');
-  if (message.content.indexOf('token.token' || 'process.env.BOT_TOKEN' || 'token') !== -1) return message.channel.send('Do you think its that easy?\nSry, but cant give you my key...');
+  if (message.content.indexOf('token.token' || 'process.env' || 'token') !== -1) return message.channel.send('Do you think its that easy?\nSry, but cant give you my key...');
   try {
     const code = args_eval.join(' ');
     let evaled = eval(code);
