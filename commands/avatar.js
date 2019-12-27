4614b9c927a969ad383357bd47254c511d61b8fe
Phil <philip.schaefer0@gmail.com>
2019-12-27 13:32:42 +0100

    recoded avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,29 +3,25 @@ const { RichEmbed } = require('discord.js');
 // creates a embed messagetemplate for failed actions
 function messageFail(client, message, body) {
   client.functions.get('FUNC_richEmbedMessage')
-    .run(client.user, message.channel, body, '', 16449540, false);
+    .run(client.user, message.channel, body, null, 16449540, false);
 }
 
 module.exports.run = async (client, message, args, config) => {
-  const [userID] = args;
+  let target;
+  if (args[0]) {
+    if (message.mentions.members.first() || message.guild.members.get(args[0])) {
+      target = message.mentions.members.first() || message.guild.members.get(args[0]);
+    } else { return messageFail(client, message, 'Sorry, but there is no user on this server with the information provided.'); }
+  } else { target = message.member; }
 
-  // TODO: check if user is part of the team
-
-  // check if user is charing a server
-  if (!await client.functions.get('FUNC_checkID').run(userID, client, 'user')) {
-    // TODO: when lookup is a function, retrieve pic from that
-    messageFail(client, message, `The user with the ID \`${userID}\` doesn't share a server with this bot or doesn't exist. Running \`${config.prefix}lookup\` command for you.`);
-    client.commands.get('lookup')
-      .run(client, message, args, config);
-    return;
-  }
-
-  const user = await client.users.find((user) => user.id === userID);
-
-  if (!user.avatarURL) messageFail(client, message, 'This user doesn\'t have a profile picture');
-
-  const embed = new RichEmbed().setImage(user.avatarURL);
-  message.channel.send({ embed });
+  message.guild.fetchMember(target)
+    .then((member) => {
+      let embed = new RichEmbed()
+        .setAuthor(member.user.tag)
+        .setColor(member.displayColor)
+        .setImage(member.user.avatarURL);
+      message.channel.send({ embed });
+    });
 };
 
 module.exports.help = {
