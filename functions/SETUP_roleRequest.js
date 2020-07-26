f6e53b0a0b4d1e25773a92a0390fe0d8d41943d1
Phil <philip.schaefer0@gmail.com>
2020-07-26 23:07:52 +0200

    removed support for multiple rolerequest channels.

--- functions/SETUP_roleRequest.js
+++ functions/SETUP_roleRequest.js
@@ -22,22 +22,20 @@ async function postReactions(message, langVarNames) {
 }
 
 module.exports.run = async (client, config) => {
-  // for each server
-  config.setup.roleRequest.channels.forEach(async (roleRequest) => {
-    if (!client.channels.get(roleRequest)) {
-      console.log(lang.log_function_SETUP_roleRequest_warn_channelMissing({
-        functionName: module.exports.help.name,
-        channelID: roleRequest,
-      }));
-      return;
-    }
-    client.channels.get(roleRequest).bulkDelete(10)
-      .catch((err) => console.log(lang.log_global_error_title(), err));
-    const langVarNames = ['prey', 'switch', 'pred', 'nsfwAccess', 'nsflAccess'];
-    let embed = await buildEmbed(config, langVarNames);
-    client.channels.get(roleRequest).send({ embed })
-      .then(async (message) => postReactions(message, langVarNames));
-  });
+  const roleRequest = config.setup.roleRequest.channel;
+  if (!client.channels.get(roleRequest)) {
+    console.log(lang.log_function_SETUP_roleRequest_warn_channelMissing({
+      functionName: module.exports.help.name,
+      channelID: roleRequest,
+    }));
+    return;
+  }
+  client.channels.get(roleRequest).bulkDelete(10)
+    .catch((err) => console.log(lang.log_global_error_title(), err));
+  const langVarNames = ['prey', 'switch', 'pred', 'nsfwAccess', 'nsflAccess'];
+  let embed = await buildEmbed(config, langVarNames);
+  client.channels.get(roleRequest).send({ embed })
+    .then(async (message) => postReactions(message, langVarNames));
 };
 
 module.exports.help = {
