fc7c61e7323473bb3302e699ba55b752e2e67216
Phil <philip.schaefer0@gmail.com>
2020-08-06 22:48:36 +0200

    disabled rolerequest

--- functions/EVENT_messageReactionAdd.js
+++ functions/EVENT_messageReactionAdd.js
@@ -1,5 +1,10 @@
 module.exports.run = async (client, reaction, user, config) => {
+  if (user.bot) return;
 
+  // rolerequest
+  // if (reaction.message.channel.id === config.setup.roleRequest.channel) {
+  //   client.functions.get('FUNC_userRoleRequest').run(client, reaction, user, config);
+  // }
 };
 
 module.exports.help = {
---
+++ functions/FUNC_userRoleRequest.js
@@ -0,0 +1,8 @@
+module.exports.run = async (client, reaction, user, config) => {
+  // config.setup.roleRequest
+  console.log(reaction);
+};
+
+module.exports.help = {
+  name: 'FUNC_userRoleRequest',
+};
