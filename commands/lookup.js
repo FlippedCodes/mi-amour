83990c8cedd6aa3ef26fce2772a4511cd62d2b00
Phil <philip.schaefer0@gmail.com>
2020-08-19 22:06:48 +0200

    repalced with new code and changed v12 stuff

--- commands/lookup.js
+++ commands/lookup.js
@@ -1,64 +1,40 @@
 const { MessageEmbed } = require('discord.js');
 
-const rp = require('request-promise');
-
 const fs = require('fs');
-
-const uri = 'https://discordapp.com/api/users/';
 
 let tokenAPI;
 
 if (fs.existsSync('./config/config.json')) {
   const api = require('../config/config.json');
-  tokenAPI = api.APItoken;
+  tokenAPI = api.token;
 } else {
-  tokenAPI = process.env.APItoken;
+  tokenAPI = process.env.BotTokenAgentBlack;
 }
 
-// TODO: make function out of command (more accessable from other commands)
-
 module.exports.run = async (client, message, args, config) => {
-  // if (!config.env.get('isTeam')) return message.react('❌');
+  if (!config.env.get('isTeam')) return message.react('❌');
 
-  let [id] = args;
+  let [userID] = args;
+  if (!userID) userID = message.author.id;
 
-  if (!id) return message.channel.send(lang.chat_command_lookup_error_missingID());
-
-  let embed = new MessageEmbed()
-    .setColor(message.member.displayColor)
-    .setFooter(client.user.tag, client.user.displayAvatarURL)
-    .setTimestamp();
-
-  let request = {
-    method: 'GET',
-    uri: `${uri}${id}`,
-    headers: {
-      Authorization: `Bot ${tokenAPI}`,
-    },
-    json: true,
-  };
-  rp(request)
-    .then((user) => {
-      let creationDate = (user.id / 4194304) + 1420070400000;
-      embed
-        .addField(lang.chat_command_lookup_embed_field_userTag(), `\`${user.username}#${user.discriminator}\``)
-        .addField(lang.chat_command_lookup_embed_field_userID(), `\`${user.id}\``)
-        .addField(lang.chat_command_lookup_embed_field_userCreationDate(), new Date(creationDate), true)
-        .setThumbnail(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`);
-      message.channel.send({ embed });
-    })
+  const embed = new MessageEmbed().setColor(message.member.displayColor);
+  const discordUser = await client.users.fetch(userID, false)
     .catch((err) => {
-      if (err.statusCode === 404) embed.setAuthor(lang.chat_command_lookup_error_noUserException());
-      else embed.setAuthor(lang.chat_command_lookup_error_unkownException_message());
-      embed.addField(lang.chat_command_lookup_error_unkownException_stopCode(), err.message);
-      message.channel.send({ embed });
+      if (err.code === 10013) embed.setAuthor('This user doesn\'t exist.');
+      else embed.setAuthor('An error occurred!');
+      embed.addField('Stopcode', err.message);
+      return message.channel.send({ embed });
     });
-  // joined servers with dates on reaction press, if to many (use .length)
-  // banned servers with dates on reaction press, if to many (use .length)
-  // userinfo through discord api
+  embed
+    .addField('Usertag', `\`${discordUser.tag}\``)
+    .addField('ID', `\`${userID}\``)
+    .addField('Account Creation Date', discordUser.createdAt, true)
+    .setThumbnail(discordUser.avatarURL);
+  message.channel.send({ embed });
 };
 
 module.exports.help = {
   name: 'lookup',
-  desc: lang.chat_command_lookup_desc(),
+  usage: 'USERID',
+  desc: 'Uses the Discord API to lookup userinformaiton',
 };
