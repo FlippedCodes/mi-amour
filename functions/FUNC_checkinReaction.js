2ee1616e6f94cf9a27fad5bfa7c2928a21dd7dca
Phil <philip.schaefer0@gmail.com>
2020-08-19 19:08:25 +0200

    inital init

---
+++ functions/FUNC_checkinReaction.js
@@ -0,0 +1,16 @@
+module.exports.run = async (client, reaction, config) => {
+  // check channel
+  if (reaction.channel_id !== config.checkin.reaction.channel) return;
+  if (reaction.message_id !== config.checkin.reaction.message) return;
+  if (reaction.emoji.name !== config.checkin.reaction.emoji) return;
+  console.log(reaction);
+  const channel = await message.guild.createChannel(args.join('_').slice(subcmd.length + 1), 'text')
+    .then((channel) => channel.setParent(config.parentRP))
+    .then((channel) => channel.lockPermissions())
+    .then((channel) => channel.overwritePermissions(message.author, { SEND_MESSAGES: true }))
+    .catch(console.log);
+};
+
+module.exports.help = {
+  name: 'FUNC_checkinReaction',
+};
