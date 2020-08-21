e8680e57ff84f380bca24717dac76d72117582b4
Phil <philip.schaefer0@gmail.com>
2020-08-22 01:56:51 +0200

    added channel creation

--- functions/FUNC_checkinReaction.js
+++ functions/FUNC_checkinReaction.js
@@ -5,12 +5,17 @@ module.exports.run = async (client, reaction, config) => {
   if (reaction.channel_id !== config.checkin.reaction.channel) return;
   if (reaction.message_id !== config.checkin.reaction.message) return;
   if (reaction.emoji.name !== config.checkin.reaction.emoji) return;
-  console.log(reaction);
-  const channel = await message.guild.createChannel(args.join('_').slice(subcmd.length + 1), 'text')
-    .then((channel) => channel.setParent(config.parentRP))
+  // console.log(reaction);
+  const username = `${reaction.member.user.username}-${reaction.member.user.discriminator}`;
+  const guild = await client.guilds.cache.find((guild) => guild.id === reaction.guild_id);
+  const user = await client.users.cache.find((user) => user.id === reaction.member.user.id);
+  // const channel = await guild.createChannel(username, 'text')
+  await guild.channels.create(username, { type: 'text' })
+    .then((channel) => channel.setParent(config.checkin.categoryID))
     .then((channel) => channel.lockPermissions())
-    .then((channel) => channel.overwritePermissions(message.author, { SEND_MESSAGES: true }))
-    .catch(console.log);
+    .then((channel) => channel.createOverwrite(user, { VIEW_CHANNEL: true }))
+    .then((channel) => channel.send('fuck you'))
+    .catch(errHander);
 };
 
 module.exports.help = {
