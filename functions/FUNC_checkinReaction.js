669f6a31a1bd1d137259c73e4e86d4e046d92e7f
Phil <philip.schaefer0@gmail.com>
2020-08-23 12:06:25 +0200

    added stuff

--- functions/FUNC_checkinReaction.js
+++ functions/FUNC_checkinReaction.js
@@ -1,20 +1,48 @@
 const errHander = (err) => { console.error('ERROR:', err); };
 
+async function getWelcomeMessage(config, user) {
+  const fs = require('fs');
+  const message = await fs.readFile('./config/abo', 'utf8', (err, data) => {
+    let output;
+    if (err) {
+      console.log(err);
+      output = `I'm sowwy, but something went wrong setting up your welcome message, <@${user.id}>. A team member is going to assist you with this.
+      
+      ~~<@&${config.teamRole}>, pls fix~~`;
+    } else output = data;
+    return output;
+  });
+  return message;
+}
+
 module.exports.run = async (client, reaction, config) => {
-  // check channel
-  if (reaction.channel_id !== config.checkin.reaction.channel) return;
-  if (reaction.message_id !== config.checkin.reaction.message) return;
-  if (reaction.emoji.name !== config.checkin.reaction.emoji) return;
-  // console.log(reaction);
-  const username = `${reaction.member.user.username}-${reaction.member.user.discriminator}`;
+  // check emoji and channel
+  const configReaction = config.checkin.reaction;
+  if (reaction.channel_id !== configReaction.channel) return;
+  if (reaction.message_id !== configReaction.message) return;
+  if (reaction.emoji.name !== configReaction.emoji) return;
+  // TODO: check if user already has checkin channel
   const guild = await client.guilds.cache.find((guild) => guild.id === reaction.guild_id);
-  const user = await client.users.cache.find((user) => user.id === reaction.member.user.id);
-  // const channel = await guild.createChannel(username, 'text')
-  await guild.channels.create(username, { type: 'text' })
+  const user = await client.users.fetch(reaction.member.user.id, false);
+  // calculate user creation
+  const currentDate = new Date();
+  const creationDate = new Date(user.createdAt);
+  const msDiff = Math.abs(currentDate - creationDate);
+  const dayDiff = Math.ceil(msDiff / (1000 * 60 * 60 * 24));
+  // TODO: add DB table to check if user was in guild before
+  // Create channel, set settings and edit channel topic
+  const topic = `
+  Username: ${user.tag}
+  Avatar: ${user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}
+  Days since creation: ${dayDiff};
+  Creation date: ${user.createdAt}`;
+  const test = await getWelcomeMessage(config, user);
+  await guild.channels.create(user.id, { type: 'text' })
     .then((channel) => channel.setParent(config.checkin.categoryID))
     .then((channel) => channel.lockPermissions())
     .then((channel) => channel.createOverwrite(user, { VIEW_CHANNEL: true }))
-    .then((channel) => channel.send('fuck you'))
+    .then((channel) => channel.setTopic(topic))
+    .then(async (channel) => channel.send(test))
     .catch(errHander);
 };
 
