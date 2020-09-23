041408e7b4ce5b25661b2e982cdb3aa3903abb8c
Phil <philip.schaefer0@gmail.com>
2020-09-23 19:41:18 +0200

    added all comamnd to help

--- commands/help.js
+++ commands/help.js
@@ -3,23 +3,30 @@ const { MessageEmbed } = require('discord.js');
 module.exports.run = async (client, message, args, config) => {
   // prepare title and user CMDs
   let embed = new MessageEmbed()
-    .setTitle(lang.chat_command_help_embed_title())
-    .setColor(message.member.displayColor)
-    .setDescription(lang.chat_command_help_embed_desc())
-    .addField(
-      lang.chat_command_help_embed_field_title({ prefix: config.prefix }),
-      lang.chat_command_help_embed_field_desc(), true,
-    );
+    // .setTitle(lang.chat_command_help_embed_title())
+    // .setDescription(lang.chat_command_help_embed_desc())
+    .setAuthor('How to uwse me:');
+  if (message.channel.type !== 'dm') embed.setColor(message.member.displayColor);
+  // creating embed fields for every command
+  client.commands.forEach((CMD) => {
+    if (!CMD.help.title) return;
+    embed.addField(CMD.help.title,
+      `\`${config.prefix}${CMD.help.name} ${CMD.help.usage || ''}\`
+      ${CMD.help.desc}`, false);
+  });
 
-  // set footer
-  embed
-    .setFooter(message.client.user.tag, message.client.user.displayAvatarURL)
-    .setTimestamp();
+  // set help command
+  // embed
+  //   .addField(
+  //     lang.chat_command_help_embed_field_title({ prefix: config.prefix }),
+  //     lang.chat_command_help_embed_field_desc(), true,
+  //   );
   message.channel.send({ embed });
   return;
 };
 
 module.exports.help = {
   name: 'help',
+  title: 'HALP',
   desc: lang.chat_command_help_desc(),
 };
