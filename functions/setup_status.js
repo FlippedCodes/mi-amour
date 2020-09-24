5d41806ae4f6dceae1ce00aee2a2c66d1091ba25
Phil <philip.schaefer0@gmail.com>
2020-09-24 20:00:29 +0200

    updated to v12

--- functions/setup_status.js
+++ functions/setup_status.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, config) => {
     console.log(`[${module.exports.help.name}] Setting status...`);
   } else return console.log(`[${module.exports.help.name}] Bot is in debugging-mode and will post the bot status-message`);
   client.user.setStatus('online');
-  const membercount = client.guilds.reduce((previousCount, currentGuild) => previousCount + currentGuild.memberCount, 0);
+  const membercount = client.guilds.cache.reduce((previousCount, currentGuild) => previousCount + currentGuild.memberCount, 0);
   client.user.setActivity(`${membercount} members in VoreNetwork`, { type: 'WATCHING' })
     .then(() => console.log(`[${module.exports.help.name}] Status set!`));
 };
