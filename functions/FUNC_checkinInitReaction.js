efe276bc43d118b0cabb6caf6a4ab4911bcda33a
Phil <philip.schaefer0@gmail.com>
2020-09-24 20:34:22 +0200

    check if channel already exists

--- functions/FUNC_checkinInitReaction.js
+++ functions/FUNC_checkinInitReaction.js
@@ -57,18 +57,22 @@ module.exports.run = async (client, reaction, config) => {
   if (reaction.channel_id !== configReaction.channel) return;
   if (reaction.message_id !== configReaction.message) return;
   if (reaction.emoji.name !== configReaction.emoji) return;
-  // TODO: check if user already has checkin channel
+  // get guild and user
   const guild = await client.guilds.cache.find((guild) => guild.id === reaction.guild_id);
   const user = await client.users.fetch(reaction.member.user.id, false);
-  const dayDiff = calcUserAge(user);
-  // TODO: add DB table to check if user was in guild before
-  // Create channel, set settings and edit channel topic
-  const topic = `
-  Username: ${user.tag}
-  Avatar: ${user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}
-  Days since creation: ${dayDiff};
-  Creation date: ${user.createdAt}`;
-  createChannel(guild, user, config, topic);
+  // check if user already has checkin channel
+  const checkinChannel = await guild.channels.cache.find((channel) => channel.name === user.id);
+  if (!checkinChannel) {
+    const dayDiff = calcUserAge(user);
+    // TODO: add DB table to check if user was in guild before
+    // Create channel, set settings and edit channel topic
+    const topic = `
+    Username: ${user.tag}
+    Avatar: ${user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}
+    Days since creation: ${dayDiff};
+    Creation date: ${user.createdAt}`;
+    createChannel(guild, user, config, topic);
+  }
   // remvove user reaction
   const reactionChannel = await guild.channels.cache.get(config.checkin.reaction.channel);
   const reactionMessage = await reactionChannel.messages.fetch(config.checkin.reaction.message);
