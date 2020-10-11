fdcf0915d0a1c9e546075c9c8d27228f9a3d0279
Phil <philip.schaefer0@gmail.com>
2020-10-11 12:57:00 +0200

    better object handling

--- functions/SETUP_roleRequest.js
+++ functions/SETUP_roleRequest.js
@@ -3,8 +3,8 @@ const { MessageEmbed } = require('discord.js');
 
 async function buildEmbed(config, roleData) {
   const embed = new MessageEmbed();
-  await Object.entries(roleData).forEach(async (reaction) => {
-    await embed.addField(reaction[1].name, reaction[1].emoji, true);
+  await roleData.forEach(async (reaction) => {
+    await embed.addField(reaction.name, reaction.emoji, true);
   });
   const result = embed
     .setTitle(lang.chat_function_SETUP_roleRequest_embed_title())
@@ -13,25 +13,25 @@ async function buildEmbed(config, roleData) {
 }
 
 async function postReactions(message, roleData) {
-  await Object.entries(roleData).forEach(async (reaction) => {
-    await message.react(await reaction[1].emoji);
+  await roleData.forEach(async (reaction) => {
+    await message.react(await reaction.emoji);
   });
 }
 
 module.exports.run = async (client, config) => {
-  const roleRequest = config.setup.roleRequest.channel;
-  if (!client.channels.get(roleRequest)) {
+  const roleRequest = config.setup.roleRequest.channelID;
+  if (!client.channels.cache.get(roleRequest)) {
     console.log(lang.log_function_SETUP_roleRequest_warn_channelMissing({
       functionName: module.exports.help.name,
       channelID: roleRequest,
     }));
     return;
   }
-  client.channels.get(roleRequest).bulkDelete(10)
+  client.channels.cache.get(roleRequest).bulkDelete(10)
     .catch((err) => console.log(lang.log_global_error_title(), err));
   const roleData = config.setup.roleRequest.roles;
   let embed = await buildEmbed(config, roleData);
-  client.channels.get(roleRequest).send({ embed })
+  client.channels.cache.get(roleRequest).send({ embed })
     .then(async (message) => postReactions(message, roleData));
 };
 
