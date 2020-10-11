663d849d991d672e49cc950104ddb2a9910e3cf5
Phil <philip.schaefer0@gmail.com>
2020-10-11 12:54:21 +0200

    inital init

---
+++ functions/TCKR_pointRemover.js
@@ -0,0 +1,8 @@
+module.exports.run = async (ID, client, type) => {
+  // interval every 5 min
+  // if time is longer then pointLifetime: remove points from user with FUNC_pointManager
+};
+
+module.exports.help = {
+  name: 'TCKR_pointRemover',
+};
