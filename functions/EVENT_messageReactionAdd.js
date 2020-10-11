874877beb9e9906f218d935d2694b7d3c25ea3a8
Phil <philip.schaefer0@gmail.com>
2020-10-11 12:55:38 +0200

    removed rolerequest check, shouldnt be handlered here

--- functions/EVENT_messageReactionAdd.js
+++ functions/EVENT_messageReactionAdd.js
@@ -2,9 +2,8 @@ module.exports.run = async (client, reaction, user, config) => {
   if (user.bot) return;
 
   // rolerequest
-  // if (reaction.message.channel.id === config.setup.roleRequest.channel) {
-  //   client.functions.get('FUNC_userRoleRequest').run(client, reaction, user, config);
-  // }
+  client.functions.get('FUNC_userRoleRequest').run(client, reaction, user, config);
+  // checkin
   client.functions.get('FUNC_checkinCompletedReaction').run(client, reaction, user, config);
 };
 
