0abdd842e77a64ea7b412c0cf07970ce43317b7a
Phil <philip.schaefer0@gmail.com>
2020-10-13 19:08:41 +0200

    changed welcome message back to default

--- functions/FUNC_checkinCompletedReaction.js
+++ functions/FUNC_checkinCompletedReaction.js
@@ -26,7 +26,8 @@ module.exports.run = async (client, reaction, user, config) => {
       // add role
       reaction.message.member.roles.add(config.checkin.checkinRole);
       // post welcome message
-      messageSuccess(client, member.guild.channels.cache.get(config.checkin.welcomeChannel), `${reaction.message.author}, you are checked-in now!\nHave a great time on the server! :3`);
+      const welcomeChannel = member.guild.channels.cache.get(config.checkin.welcomeChannel);
+      welcomeChannel.send(`${reaction.message.author}, you are checked-in now!\nHave a great time on the server! :3`);
       // delete channel
       reaction.message.channel.delete();
       return;
