a4337c5881ebd2b41247bd1c5e53e12ac8a3cc35
Phil <philip.schaefer0@gmail.com>
2020-11-30 23:32:02 +0100

    completed code

--- functions/FUNC_disboardBoost.js
+++ functions/FUNC_disboardBoost.js
@@ -3,9 +3,22 @@ module.exports.run = async (client, message, config) => {
   if (message.author.id !== config.disboard.userID) return;
   if (message.channel.id !== config.disboard.channelID) return;
   // check if message is a successful boost
-  console.log(message);
-  // remote all members from role
+  const recievedEmbed = message.embeds[0];
+  const desc = recievedEmbed.description;
+  if (!desc.startsWith('<@')) return;
+  if (recievedEmbed.hexColor !== config.disboard.successfulHex) return;
+
+  const role = await message.guild.roles.fetch(config.disboard.lastBoostedRoleID);
+  // remove all members from role
+  role.members.forEach((member) => member.roles.remove(role));
   // assign role to new member
+  const userID = desc.substring(
+    desc.lastIndexOf('@') + 1,
+    desc.lastIndexOf('>'),
+  );
+  if (!userID) return;
+  const member = await message.guild.member(userID);
+  member.roles.add(role);
 };
 
 module.exports.help = {
