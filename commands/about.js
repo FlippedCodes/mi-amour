05cfc0a039b6d6568453a6bdd6b779cd5e396ebf
Phil <philip.schaefer0@gmail.com>
2022-08-11 03:23:26 +0200

    [update] [remove] replaced with code from nicashee

---
+++ commands/about.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder } = require('discord.js');
+
+const fs = require('fs');
+
+module.exports.run = async (interaction) => {
+  fs.readFile(config.aboutText, 'utf8', (err, content) => {
+    if (err) {
+      console.log(err);
+      messageFail(interaction, 'Oh, no! Something went wrong. Sorry about that :(');
+      return;
+    }
+    const embed = new EmbedBuilder();
+    embed.setDescription(content)
+      .setColor('Orange')
+      .setTitle('About');
+    reply(interaction, { embeds: [embed] });
+  });
+};
+
+module.exports.data = new CmdBuilder()
+  .setName('about')
+  .setDescription('Learn more about me!');
