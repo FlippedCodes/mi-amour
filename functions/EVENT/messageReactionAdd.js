90331d8c1b9a3e7f9d463dcbfd36179eed308bb6
Phil <philip.schaefer0@gmail.com>
2022-08-22 14:05:52 +0200

    [remove] deprecated funciton

--- functions/EVENT/messageReactionAdd.js
+++ functions/EVENT/messageReactionAdd.js
@@ -6,8 +6,6 @@ module.exports.run = async (reaction, user) => {
 
   // rolerequest
   if (reaction.message.channel.id === config.setup.roleRequest.channelID) return client.functions.get('ENGINE_roleRequest_manager').run(reaction, user);
-  // checkin
-  if (reaction.message.channel.parentId === config.checkin.categoryID) return client.functions.get('ENGINE_checkin_completedReaction').run(reaction, user);
 };
 
 module.exports.data = {
