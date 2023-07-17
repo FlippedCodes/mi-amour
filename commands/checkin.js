d9a4659af4fa2aa6093bbb6e8a3d541f487db0ae
Phil <philip.schaefer0@gmail.com>
2023-07-17 20:44:08 +0200

    add - command feedback

--- commands/checkin.js
+++ commands/checkin.js
@@ -3,7 +3,8 @@ module.exports.run = async (interaction) => {
   if (!interaction.member.roles.cache.find(({ id }) => id === config.teamRole)) return messageFail(interaction, 'You don\'t have access to this command! òwó');
 
   if (interaction.channel.parentId !== config.checkin.categoryID) return messageFail(interaction, 'This channel is not a checkin channel.');
-  return client.functions.get('ENGINE_checkin_postReaction').run(interaction);
+  await client.functions.get('ENGINE_checkin_postReaction').run(interaction);
+  messageSuccess(interaction, 'Checkin posted.');
 };
 
 module.exports.data = new CmdBuilder()
