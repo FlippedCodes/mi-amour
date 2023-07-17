c4643bfc4256e02e976aa0d0559dfb740e977cab
Phil <philip.schaefer0@gmail.com>
2023-07-17 20:41:41 +0200

    fix - interaciton doesnt have a message object

--- commands/checkin.js
+++ commands/checkin.js
@@ -2,9 +2,8 @@ module.exports.run = async (interaction) => {
   // check if user is teammember
   if (!interaction.member.roles.cache.find(({ id }) => id === config.teamRole)) return messageFail(interaction, 'You don\'t have access to this command! òwó');
 
-  const message = interaction.message;
-  if (message.channel.parentId !== config.checkin.categoryID) return messageFail(interaction, 'This channel is not a checkin channel.');
-  return client.functions.get('ENGINE_checkin_postReaction').run(message);
+  if (interaction.channel.parentId !== config.checkin.categoryID) return messageFail(interaction, 'This channel is not a checkin channel.');
+  return client.functions.get('ENGINE_checkin_postReaction').run(interaction);
 };
 
 module.exports.data = new CmdBuilder()
