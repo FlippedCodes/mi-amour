bdd667abf27f3f2b3d4ad0c22e73ff368d755e8f
Phil <philip.schaefer0@gmail.com>
2023-08-23 23:47:30 +0200

    update-remove messages in open channels

--- functions/EVENT/messageCreate.js
+++ functions/EVENT/messageCreate.js
@@ -4,6 +4,8 @@ module.exports.run = async (message) => {
   if (message.channel.type === 'dm') return;
   if (message.guild.id !== config.guildId) return;
 
+  if (message.channel.parentId === config.openChannelCategory) return message.delete();
+
   if (config.contentWarning.checkChannels.includes(message.channel.id)) return client.functions.get('ENGINE_contentWarning_check').run(message);
 
   if (config.linkReplace.checkChannels.includes(message.channel.id)) return client.functions.get('ENGINE_linkReplace_check').run(message);
