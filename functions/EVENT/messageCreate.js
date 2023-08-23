75bff6f0757efcfdce4c25effbba55fed6168d3b
Phil <philip.schaefer0@gmail.com>
2023-08-23 23:46:56 +0200

    upy apply to own guild

--- functions/EVENT/messageCreate.js
+++ functions/EVENT/messageCreate.js
@@ -2,6 +2,7 @@ module.exports.run = async (message) => {
   // return if unwanted
   if (message.author.bot) return;
   if (message.channel.type === 'dm') return;
+  if (message.guild.id !== config.guildId) return;
 
   if (config.contentWarning.checkChannels.includes(message.channel.id)) return client.functions.get('ENGINE_contentWarning_check').run(message);
 
